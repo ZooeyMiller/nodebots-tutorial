37481549241945e2eeb805dc50af1ec31a0b1ec4
Zooey <zooey@lbk.org.uk>
2017-03-22 22:56:27 +0000

    solved ex2

---
+++ ex2.js
@@ -0,0 +1,10 @@
+const five = require('johnny-five');
+const board = new five.Board();
+board.on('ready', () => {
+  const servo = new five.Servo(9);
+  servo.sweep();
+  board.wait(3000, () => {
+    servo.stop();
+    servo.center();
+  });
+});
