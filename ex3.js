e67d181d1022f29832b201b337693bdd0a2fcf6e
Zooey <zooey@lbk.org.uk>
2017-03-22 23:07:42 +0000

    completed ex3

---
+++ ex3.js
@@ -0,0 +1,17 @@
+const five = require('johnny-five');
+
+const board = new five.Board();
+board.on('ready', () => {
+  const motor = new five.Motor(9);
+  motor.start(200);
+  function spinWait() {
+    board.wait(2000, () => {
+      motor.stop();
+      board.wait(1000, () => {
+        motor.start(200);
+        spinWait();
+      });
+    });
+  }
+  spinWait();
+});
